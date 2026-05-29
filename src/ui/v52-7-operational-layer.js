66eeda2c681e573e84748d8113d36dddb8bc5015
MJC <max@maximumjusticecybersecurity.com>
2026-05-29 15:33:42 -0400

    ui: load V55.1 TrustMap overlay

--- src/ui/v52-7-operational-layer.js
+++ src/ui/v52-7-operational-layer.js
@@ -2,3 +2,4 @@ import './v53-trust-model-spines.js';
 import './v53-metadata-patch.js';
 import './v54-2-enterprise-trust-universe.js';
 import './v55-purpose-protocol-constraint-layer.js';
+import './v55-1-trustmap-radar-constellation.js';
