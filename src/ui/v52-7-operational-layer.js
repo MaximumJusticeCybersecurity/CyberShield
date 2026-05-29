c6f0e852fbdb0497399f002bf79e625789fce2fd
MJC <max@maximumjusticecybersecurity.com>
2026-05-29 16:05:41 -0400

    ui: load V55.2 TrustMap constellation polish

--- src/ui/v52-7-operational-layer.js
+++ src/ui/v52-7-operational-layer.js
@@ -3,3 +3,4 @@ import './v53-metadata-patch.js';
 import './v54-2-enterprise-trust-universe.js';
 import './v55-purpose-protocol-constraint-layer.js';
 import './v55-1-trustmap-radar-constellation.js';
+import './v55-2-trustmap-constellation-polish.js';
