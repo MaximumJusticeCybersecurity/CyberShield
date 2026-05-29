a8eeca253d6fcdb1b16fcfbaa83078026f474523
MJC <max@maximumjusticecybersecurity.com>
2026-05-29 13:41:08 -0400

    ui: load V54.2 Enterprise Trust Universe

--- src/ui/v52-7-operational-layer.js
+++ src/ui/v52-7-operational-layer.js
@@ -1,4 +1,3 @@
 import './v53-trust-model-spines.js';
 import './v53-metadata-patch.js';
-import './v54-enterprise-trustmap-kernel.js';
-import './v54-1-trustmap-collision-control.js';
+import './v54-2-enterprise-trust-universe.js';
