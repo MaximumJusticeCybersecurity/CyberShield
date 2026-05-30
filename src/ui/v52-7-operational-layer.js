70b461ebc6bd81e3c285b668f00cf2ac6de92ac8
MJC <max@maximumjusticecybersecurity.com>
2026-05-30 10:09:12 -0400

    Wire V58 Operational Trust Control Pane

--- src/ui/v52-7-operational-layer.js
+++ src/ui/v52-7-operational-layer.js
@@ -16,3 +16,4 @@ import './v56-3-universal-score-contract.js';
 import './v56-4-core-trust-scoring-models.js';
 import './v57-model-driven-proof-pack.js';
 import './v57-1-board-executive-narrative.js';
+import './v58-operational-trust-control-pane.js';
