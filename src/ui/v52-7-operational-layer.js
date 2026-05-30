30bfe3afdfa7552914b9ba30af3ecd109223fdd9
MJC <max@maximumjusticecybersecurity.com>
2026-05-30 12:04:10 -0400

    Wire V59.4 Internet Trust board narrative

--- src/ui/v52-7-operational-layer.js
+++ src/ui/v52-7-operational-layer.js
@@ -23,3 +23,4 @@ import './v59-internet-trust-engine-mvp-scaffold.js';
 import './v59-1-artifact-intake-claim-table.js';
 import './v59-2-claim-model-proof-trace.js';
 import './v59-3-internet-trust-proof-pack-export-contract.js';
+import './v59-4-internet-trust-board-narrative.js';
