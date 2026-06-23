09457d0e239601336a043b20a4284559655b2afd
MJC <max@maximumjusticecybersecurity.com>
2026-06-23 16:23:57 -0400

    Load Vercel Analytics on vendor-risk routes

--- src/atdr/report-capture-config.js
+++ src/atdr/report-capture-config.js
@@ -5,6 +5,8 @@
 // This URL is safe to expose in static front-end code. Never place Google
 // credentials, API keys, service-account material, or secrets in front-end code.
 
+import '../vercel-analytics.js';
+
 const REPORT_CAPTURE_ENDPOINT = 'https://script.google.com/macros/s/AKfycbypKJ4PBpqtthGURqr4mebXMD6Sdv-BmbRIgwulQmewl6SSyVNx_cX5xbsh3HH6Xs5r/exec';
 const CRM_SHEET_ID = '1SDfqw-rRuluqBdPUT6Ex4UIajO-CCEtny84OTMKhQ3w';
 const REPORT_CAPTURE_MODE = REPORT_CAPTURE_ENDPOINT ? 'configured' : 'simulated';
